d7aae26c70053da17402ce9a8079a6654bf19c32
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2017-06-15 19:46:30 +0200

    Update content

--- _plugins/jekyll_asset_pipeline.rb
+++ _plugins/jekyll_asset_pipeline.rb
@@ -3,4 +3,15 @@ require 'jekyll_asset_pipeline'
 # Add Yahoo's YUI Compressor
 # https://github.com/matthodan/jekyll-asset-pipeline#yahoos-yui-compressor
 module JekyllAssetPipeline
+	class SassConverter < JekyllAssetPipeline::Converter
+    require 'sass'
+
+    def self.filetype
+      '.scss'
+    end
+
+    def convert
+      return Sass::Engine.new(@content, syntax: :scss).render
+    end
+  end
 end
